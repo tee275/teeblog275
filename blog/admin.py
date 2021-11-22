8f3a3678039e1c183057975fb42452c3022fe897
Tien <minhtien275@gmail.com>
2021-11-22 14:03:39 +0700

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Post, Comment
+
+# Register your models here.
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'date']
+    list_filter = ['date']
+    search_fields = ['title',]
+    inlines = [CommentInline]
+ 
+admin.site.register(Post, PostAdmin)
